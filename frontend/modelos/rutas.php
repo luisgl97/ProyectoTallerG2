66a02d7a62a44612933e3eca9b91b0f31351268c
luisgonzales97 <luisaltlv2010@gmail.com>
2021-04-02 18:27:34 -0500

    1version

--- frontend/modelos/rutas.php
+++ frontend/modelos/rutas.php
@@ -8,7 +8,7 @@ class Ruta{
 
 	public function ctrRuta(){
 
-		return "http://localhost/frontend/";
+		return "http://localhost/games-4-life/frontend/";
 	
 	}
 
@@ -18,7 +18,7 @@ class Ruta{
 
 	public function ctrRutaServidor(){
 
-		return "http://localhost/backend/";
+		return "http://localhost/games-4-life/backend/";
 	
 	}
 
--- proyecto-amauta/profesores.php
+++
@@ -1,8 +0,0 @@
-<?php include_once 'includes/templates/header.php'; ?>
-
-
-<?php include_once 'includes/templates/profesores.php'; ?>
-    
-
-
-    <?php include_once 'includes/templates/footer.php'; ?>
