fbaafaac037fc59ef162571ed3519c2377703637
luisgonzales97 <luisaltlv2010@gmail.com>
2021-04-09 22:32:56 -0500

    je

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9ee0e4138ec1c282cc8aca6ce3279a6a0b174114',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9ee0e4138ec1c282cc8aca6ce3279a6a0b174114',
+    ),
+  ),
+);
